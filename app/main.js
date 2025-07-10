044f7a27d8f2a10656d82c3f2f4039025a193818
Ayan Khan <beaayank@gmail.com>
2025-07-10 12:56:09 +0530

    [12.Include DescribeTopicPartitions in APIVersions(5)]

--- app/main.js
+++ app/main.js
@@ -1,75 +1,35 @@
-import { sendResponseMessage } from "./utils/index.js";
+import net from "net";
 
-export const handleDescribeTopicPartitionsRequest = (
-  connection,
-  responseMessage,
-  buffer,
-) => {
-  const clientLength = buffer.subarray(12, 14);
-  const clientLengthValue = clientLength.readInt16BE();
-  const tagBuffer = Buffer.from([0]);
-  const throttleTimeMs = Buffer.from([0, 0, 0, 0]);
-  const errorCode = Buffer.from([0, 3]);
-  const topicId = Buffer.from(new Array(16).fill(0));
-  const topicAuthorizedOperations = Buffer.from("00000df8", "hex");
+import { sendResponseMessage, pick } from "./utils/index.js";
+import { handleApiVersionsRequest } from "./api_versions_request.js";
+import { parseRequest } from "./request_parser.js";
+import { handleDescribeTopicPartitionsRequest } from "./describe_topic_partitions_request.js";
 
-  let updatedResponse = {
-    correlationId: responseMessage.correlationId,
-    tagBuffer,
-    throttleTimeMs,
-  };
+const server = net.createServer((connection) => {
+  connection.on("data", (buffer) => {
+    const { messageSize, requestApiKey, requestApiVersion, correlationId } =
+      parseRequest(buffer);
 
-  const topicArrayLength =
-    buffer.subarray(clientLengthValue + 15, clientLengthValue + 16).readInt8() -
-    1;
+    const responseMessage = {
+      messageSize,
+      requestApiKey,
+      requestApiVersion,
+      correlationId,
+    };
 
-  let topicIndex = clientLengthValue + 16;
-  const topics = new Array(topicArrayLength).fill(0).map((_) => {
-    const topicLength = buffer.subarray(topicIndex, topicIndex + 1);
-    topicIndex += 1;
-    const topicName = buffer.subarray(
-      topicIndex,
-      topicIndex + topicLength.readInt8() - 1,
-    );
+    const requestVersion = requestApiVersion.readInt16BE();
 
-    topicIndex += topicLength.readInt8() - 1;
-    return [topicLength, topicName];
+    if (requestApiKey.readInt16BE() === 18) {
+      handleApiVersionsRequest(connection, responseMessage, requestVersion);
+    } else if (requestApiKey.readInt16BE() === 75) {
+      handleDescribeTopicPartitionsRequest(connection, responseMessage, buffer);
+    } else {
+      sendResponseMessage(
+        connection,
+        pick(responseMessage, "messageSize", "correlationId"),
+      );
+    }
   });
+});
 
-  updatedResponse.topicLength = Buffer.from([topics.length + 1]);
-  topics.forEach(([topicLength, topicName], index) => {
-    updatedResponse[`${index}topicName`] = Buffer.concat([
-      errorCode,
-      topicLength,
-      topicName,
-      topicId,
-      Buffer.from([0]),
-      Buffer.from([1]),
-      topicAuthorizedOperations,
-      tagBuffer,
-    ]);
-  });
-
-  const responsePartitionLimitIndex = topicIndex + 1;
-  const _responsePartitionLimit = buffer.subarray(
-    responsePartitionLimitIndex,
-    responsePartitionLimitIndex + 4,
-  );
-  const cursorIndex = responsePartitionLimitIndex + 4;
-  const cursor = buffer.subarray(cursorIndex, cursorIndex + 1);
-  updatedResponse = { ...updatedResponse, cursor, cursortagbuffer: tagBuffer };
-
-  const messageSize = Buffer.from([
-    0,
-    0,
-    0,
-    Buffer.concat(Object.values(updatedResponse)).length,
-  ]);
-  updatedResponse = {
-    messageSize,
-    ...updatedResponse,
-  };
-
-  console.log("messageSize", messageSize.readInt32BE());
-  sendResponseMessage(connection, updatedResponse);
-};
+server.listen(9092, "127.0.0.1");
